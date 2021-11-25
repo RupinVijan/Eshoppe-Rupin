dcbda6f4d51fcb924dd31fe63ea8de7a4e2fc28f
RupinVijan <rupinvijan@gmail.com>
2021-11-25 22:11:34 +0530

    v1 day2

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const errorMiddleware = require('./middleware/error')
+
 const app= express();
 
 app.use(express.json());
+
+app.use(errorMiddleware);
 
 app.use('/api/v1',require('./routes/products'))
 
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,13 +1,59 @@
 const productSchema = require('../models/productmodel')
+const Errorhandler = require('../utils/errorHandler')
 
 exports.createProduct = async(req,res,next)=>{
     const product= await productSchema.create(req.body)
     res.status(201).json({
-        sucess:true,
+        success:true,
         product
     })
 }
 
-exports.getAllProducts = (req,res)=>{
-    res.status(200).send({message:"route is working fine"})
+exports.getProductById= async (req,res,next) =>{
+    let product = await productSchema.findById(req.params.id)
+    if(!product){
+        return next(new Errorhandler("product not found",404))
+}
+    res.status(200).send({
+        success:true,
+        product
+    })
+}
+
+exports.getAllProducts = async(req,res)=>{
+    const product = await productSchema.find();
+    res.status(200).send({success:true,product})
+}
+
+exports.updateProduct = async(req,res,next)=>{
+    let product = await productSchema.findById(req.params.id)
+    if(!product){
+        res.status(500).send({
+            success:false,
+            message:"product not Found"
+        })}
+    product =await productSchema.findByIdAndUpdate(req.params.id,req.body,{
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+    res.status(200).send({
+        success:true,
+        product
+    })   
+}
+
+exports.deleteProduct = async (req,res,next) =>{
+    const product = await productSchema.findById(req.params.id);
+    if(!product){
+        res.status(500).send({
+            success:false,
+            message:"Product not found"
+        })
+    }
+    await productSchema.findByIdAndDelete(req.params.id)
+    res.status(201).send({
+        success:true,
+        message:"Product Deleted Successfully."
+    })
 }
